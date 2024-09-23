afebf7ff5876f48d46fccec3bb1dde05d863f4b4
franfervaz03@gmail.com <franfervaz03@gmail.com>
2024-09-23 10:10:38 +0200

    Ejercicio terminado

---
+++ 01-basics/ut-2t/exercise-1.js
@@ -0,0 +1,11 @@
+console.log("%cWellcome to the application", "color:blue");
+console.log("%cThis is an informational message.", "color:green");
+console.warn("This is a warning. Be cautions.");
+console.log("Errors! Sommething went wrong.");
+
+const NAME1 = "Jhon";
+const NAME2 = "Jane";
+const NAME3 = "Bob";
+
+const PEOPLE = [[NAME1, 30, "New York"],[NAME2, 25, "San Francisco"],[NAME3, 40, "Chicago"]];
+console.table(PEOPLE);
