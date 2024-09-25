a229b70c6e8938aa6cdf09ada3b0f2460ba20401
franfervaz03@gmail.com <franfervaz03@gmail.com>
2024-09-25 09:01:26 +0200

    segundo ejercicio

---
+++ 02-basisc/ut-2t/exercise-2.js
@@ -0,0 +1,28 @@
+//Declaracion de las constantes
+const MSGALERT = "This is am alert message, click me to move on";
+const MSGALERTCONFIRM =
+  "Show me in an alert if you clicked on ok or false\nEither OK or Cancel.";
+const MSGALERTTRUE = "You clicked on true click me to move on!!!";
+const MSGALERTFALSE = "You clicked on false click me to move on!!!";
+const MSGPROMPT = "Show me in an alert the message typed";
+const MSGPROMPTALERT = "You typed ";
+const MSGPROMPTALERT2 = " click me to move on!!!"
+
+//Declaracion del evento que crea alerts, prompt y confirm
+let iniciarEvento = () => {
+  alert(MSGALERT);
+
+  if (confirm(MSGALERTCONFIRM) == true) {
+    alert(MSGALERTTRUE);
+  } else {
+    alert(MSGALERTFALSE);
+  }
+
+  let acept = prompt(MSGPROMPT);
+  if (acept != null) {
+    alert(MSGPROMPTALERT + acept+MSGPROMPTALERT2);
+  }
+};
+
+//evente que cuando inicia la pagina carga el evento.
+document.addEventListener("DOMContentLoaded", iniciarEvento(), true);
