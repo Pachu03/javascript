eabfe3e3d2fc13d44461ce25c6c92de7e24e5cd0
Tu Nombre <tu@ejemplo.com>
2024-10-03 09:30:13 +0200

    cambios de directorio y de codigo js

--- 02-DOM/ut3-vallinajs-inserts/main.js
+++ 02-DOM/ut3-vallinajs-inserts/main.js
@@ -3,16 +3,17 @@ const texts = [
   "This is a paragraph",
   "This is a another paragraph",
 ];
+
 let img = document.createElement("img");
-img.src = "assests/img_la.jpg";
-
 let h1 = document.createElement("h1");
-h1.textContent = texts[0];
-
 let p = document.createElement("p");
-p.textContent = texts[1];
-
 let p2 = document.createElement("p");
+
+img.src = "assests/img_la.jpg";
+img.alt = "LA";
+
+h1.textContent = texts[0];
+p.textContent = texts[1];
 p2.textContent = texts[2];
 
 document.body.appendChild(img);
