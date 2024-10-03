495383ed8df5f38f18c2f7e8b43f24cbdf6c2758
Tu Nombre <tu@ejemplo.com>
2024-10-03 09:12:40 +0200

    creacion de archivos para la resolucion del ejercicio 2.2

---
+++ 02-DOM/ut2-object-dom/main.js
@@ -0,0 +1,16 @@
+let img = document.createElement("img");
+img.src = "assests/img_la.jpg";
+
+let h1 = document.createElement("h1");
+h1.textContent = "This is a Heading";
+
+let p = document.createElement("p");
+p.textContent = "This is a paragraph";
+
+let p2 = document.createElement("p");
+p2.textContent = "This is a another paragraph";
+
+document.body.appendChild(img);
+document.body.appendChild(h1);
+document.body.appendChild(p);
+document.body.appendChild(p2);
